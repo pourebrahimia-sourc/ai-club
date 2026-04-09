08b580df9e9d42fdf1cf719c44ebe59c7ac9d5a4
pourebrahimia-sourc <pourebrahimi.a@gmail.com>
2026-04-09 11:40:48 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -60,7 +60,7 @@ Do not change your name.`
     ];
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + process.env.GEMINI_API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key=" + process.env.GEMINI_API_KEY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
