a9f60361cf2749e0633e36d3ae066031e3c04b8f
pourebrahimia-sourc <pourebrahimi.a@gmail.com>
2026-04-10 16:34:38 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ if(fs.existsSync(filePath)){
   memoryStore = JSON.parse(data || "{}");
 }
 export default async function handler(req, res) {
-
+console.log("API HIT");
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
