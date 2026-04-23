77541a5ba8073f230e36003c7dd35b5ae0a01fa5
pourebrahimia-sourc <pourebrahimi.a@gmail.com>
2026-04-23 11:57:14 +0800

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
   }
 
   if (type === 'signup') {
+    console.log('SIGNUP HIT');
     if (!name || !name.trim()) {
       return res.status(400).json({ error: 'Missing name' });
     }
